9745d82efad9926239e2431e52b5cf653b88e16a
PouyaAfrashteh <avazpedia.master@gmail.com>
2023-12-12 19:27:11 +0330

    main text contents created

--- scripts/investment.js
+++ scripts/investment.js
@@ -1,4 +1,22 @@
-document.querySelector(".ham-menu").addEventListener("click", function () {
-  this.classList.toggle("active");
-  document.querySelector(".sliding-menu").classList.toggle("open-slide");
+const blurry = document.querySelector("#blur");
+const hamburger = document.querySelector(".ham-menu");
+const slideMenu = document.querySelector(".sliding-menu");
+const closeMenu = function () {
+  slideMenu.classList.remove("open-slide");
+  blurry.classList.add("hidden");
+  hamburger.classList.remove("active");
+};
+//opening menu by pressing ham button
+hamburger.addEventListener("click", function () {
+  this.classList.add("active");
+  slideMenu.classList.add("open-slide");
+  blurry.classList.remove("hidden");
+});
+// closing menu by clicking on blurry layer
+blurry.addEventListener("click", function () {
+  closeMenu();
+});
+//closing slide-menu by pressing Esc
+document.addEventListener("keydown", function (keyPress) {
+  keyPress.key === "Escape" && closeMenu();
 });
