8b4b19f5d43885e0176b5257c66ccc9f3e44f807
PouyaAfrashteh <avazpedia.master@gmail.com>
2024-01-12 21:32:24 +0330

    contact-us done

---
+++ scripts/contact_us.js
@@ -0,0 +1,25 @@
+const blurry = document.querySelector("#blur");
+const hamburger = document.querySelector(".ham-menu");
+const slideMenu = document.querySelector(".sliding-menu");
+const closeMenu = function () {
+  slideMenu.classList.remove("open-slide");
+  blurry.classList.add("hidden");
+  hamburger.classList.remove("active");
+};
+//opening menu by pressing ham button
+hamburger.addEventListener("click", function () {
+  this.classList.add("active");
+  slideMenu.classList.add("open-slide");
+  blurry.classList.remove("hidden");
+});
+// closing menu by clicking on blurry layer
+blurry.addEventListener("click", function () {
+  closeMenu();
+});
+//closing slide-menu by pressing Esc
+document.addEventListener("keydown", function (keyPress) {
+  keyPress.key === "Escape" && closeMenu();
+});
+document.querySelector("#checkbox").addEventListener("click", function () {
+  this.classList.toggle("appearance-none");
+});
